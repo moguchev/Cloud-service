0e5857205d07d737bdd4b9379cad9f93e0a78003
AlexeyZaharov <32190965+AlexeyZaharov@users.noreply.github.com>
2019-04-08 14:11:42 +0300

    Create architecture.hpp

---
+++ include/architecture.hpp
@@ -0,0 +1,55 @@
+// Copyright 2019 (c) <Cloud9>
+
+#pragma once
+#include <any>
+#include <map>
+#include <string>
+
+class Resolver;
+
+class AbstractClient {
+public:
+    virtual void login(
+        const std::string& login,
+        const std::string& password) = 0;
+    virtual void logout() = 0;
+    virtual void run(const std::string& command) = 0;
+    virtual void set_resolver(const Resolver* res) = 0;
+    virtual ~AbstractClient() = 0;
+private:
+    Resolver* resolver_ = nullptr;
+};
+
+class ConsoleClient : public  AbstractClient {
+public:
+    void write_to_console();
+    void read_from_console();
+};
+
+class Resolver {
+public:
+    virtual void handle_command(const std::string& command) = 0;
+    virtual std::string get_answer() = 0;
+    virtual ~Resolver() = 0;
+};
+
+class RestClient {
+public:
+    virtual std::string handle_request() = 0;
+    virtual std::string handle_response() = 0;
+    virtual ~RestClient() = 0;
+};
+
+class Request {
+public:
+    virtual std::string get_body() = 0;
+    virtual std::string set_body() = 0;
+    virtual ~Request() = 0;
+};
+
+class Response {
+public:
+    virtual std::string get_body() = 0;
+    virtual std::string set_body() = 0;
+    virtual ~Response() = 0;
+};
