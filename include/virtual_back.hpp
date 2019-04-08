d0f17a7bffafe6b72843a036ec3ef3043fb3026a
AlexeyZaharov <32190965+AlexeyZaharov@users.noreply.github.com>
2019-04-08 15:04:15 +0300

    Update virtual_back.hpp

--- include/virtual_back.hpp
+++ include/virtual_back.hpp
@@ -2,26 +2,25 @@
 
 #ifndef INCLUDE_VIRTUAL_BACK_HPP_
 #define INCLUDE_VIRTUAL_BACK_HPP_
-#include <map>
 #include <any>
 
 class DataBase {
 public:
-    virtual std::any get(const std::any& data) = 0;
-    virtual void make_note(const std::any& root, const std::any& data) = 0;
+    virtual std::any get(const std::any& data) const = 0;
+    virtual void make_note(const std::any& root, const std::any& data) const = 0;
     virtual void delete_note(const std::any& str) = 0;
     virtual ~DataBase() = 0;
 };
 
 class Command {
 public:
-    virtual void execute(const DataBase* database) = 0;
+    virtual void execute(const DataBase* database) const = 0;
     virtual ~Command() = 0;
 };
 
 class Receiver {
 public:
-    virtual void operator()(Command* command) = 0;
+    virtual void operator()(Command* command) const = 0;
     virtual ~Receiver() = 0;
 };
 
