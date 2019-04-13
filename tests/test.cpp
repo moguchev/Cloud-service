556b62a245790ed1a3c778feb5f10fa6740e3afe
AlexeyZaharov <32190965+AlexeyZaharov@users.noreply.github.com>
2019-04-13 11:52:30 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -10,7 +10,8 @@ TEST(database, good) {
     myDataBase.makeNote(root, data);
     Note answer = myDataBase.get(root);
 
-    EXPECT_EQ(std::any_cast<std::string>(answer.GetData()), std::any_cast<std::string>(data.GetData()));
+    EXPECT_EQ(std::any_cast<std::string>(answer.GetData()),
+              std::any_cast<std::string>(data.GetData()));
 }
 
 TEST(database, bad) {
@@ -20,7 +21,8 @@ TEST(database, bad) {
     myDataBase.makeNote(root, data);
     Note answer = myDataBase.get("logout");
 
-    EXPECT_EQ(std::any_cast<std::string>(answer.GetData()), std::string(""));
+    EXPECT_EQ(std::any_cast<std::string>(answer.GetData()),
+              std::string(""));
 }
 
 TEST(database, delete_note) {
@@ -29,11 +31,13 @@ TEST(database, delete_note) {
 
     myDataBase.makeNote(root, data);
     Note answer = myDataBase.get("login");
-    EXPECT_EQ(std::any_cast<std::string>(answer.GetData()), std::any_cast<std::string>(data.GetData()));
+    EXPECT_EQ(std::any_cast<std::string>(answer.GetData()),
+              std::any_cast<std::string>(data.GetData()));
 
     myDataBase.deleteNote(root);
     answer = myDataBase.get("login");
-    EXPECT_EQ(std::any_cast<std::string>(answer.GetData()), std::string(""));
+    EXPECT_EQ(std::any_cast<std::string>(answer.GetData()),
+              std::string(""));
 }
 
 TEST(receiver, execute) {
@@ -43,5 +47,6 @@ TEST(receiver, execute) {
 
     Note answer = myDataBase.get("login");
 
-    EXPECT_EQ(std::any_cast<std::string>(answer.GetData()), std::string("mail"));
+    EXPECT_EQ(std::any_cast<std::string>(answer.GetData()),
+              std::string("mail"));
 }
