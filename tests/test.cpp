b5668671b7a0cf3281cc6dc8b52fd5b62454eeee
Alexey <ljoha1999@mail.ru>
2019-04-29 19:07:48 +0300

    test

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,10 +4,9 @@
 #include <../include/back.hpp>
 
 TEST(database, good) {
-    std::string root = "login";
-    //Note data("mail");
+    std::string root = std::string("login");
     Note data;
-    data = std::any("mail");
+    data = std::any(std::string("mail"));
 
     myDataBase.makeNote(root, data);
     Note* answer = myDataBase.get(root);
@@ -17,39 +16,39 @@ TEST(database, good) {
 }
 
 TEST(database, bad) {
-    std::string root = "login";
+    std::string root = std::string("login");
     Note data;
-    data = std::any("mail");
+    data = std::any(std::string("mail"));
 
     myDataBase.makeNote(root, data);
-    Note* answer = myDataBase.get("logout");
+    Note* answer = myDataBase.get(std::string("logout"));
 
     EXPECT_EQ(std::any_cast<std::string>(answer->GetData()),
               std::string(""));
 }
 
 TEST(database, delete_note) {
-    std::string root = "login";
+    std::string root = std::string("login");
     Note data;
-    data = std::any("mail");
+    data = std::any(std::string("mail"));
 
     myDataBase.makeNote(root, data);
-    Note* answer = myDataBase.get("login");
+    Note* answer = myDataBase.get(std::string("login"));
     EXPECT_EQ(std::any_cast<std::string>(answer->GetData()),
               std::any_cast<std::string>(data.GetData()));
 
     myDataBase.deleteNote(root);
-    answer = myDataBase.get("login");
+    answer = myDataBase.get(std::string("login"));
     EXPECT_EQ(std::any_cast<std::string>(answer->GetData()),
               std::string(""));
 }
 
 TEST(receiver, execute) {
-    Load load("login", "mail");
+    Load load(std::string("login"), std::string("mail"));
 
     myReceiver(&load);
 
-    Note* answer = myDataBase.get("login");
+    Note* answer = myDataBase.get(std::string("login"));
 
     EXPECT_EQ(std::any_cast<std::string>(answer->GetData()),
               std::string("mail"));
