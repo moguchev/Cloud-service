19f01940579063996c635b00159cb7a07827d0a4
moguchev <abracadabra.14@mail.ru>
2019-05-26 12:29:52 +0300

    delete CRTMemory checks

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,7 +1,7 @@
 // Copyright 2019 (c) <Cloud9>
 #define _SILENCE_CXX17_ALLOCATOR_VOID_DEPRECATION_WARNING
-#define _CRTDBG_MAP_ALLOC
-#define new new( _NORMAL_BLOCK, __FILE__, __LINE__)
+// #define _CRTDBG_MAP_ALLOC
+// #define new new( _NORMAL_BLOCK, __FILE__, __LINE__)
 #include <stdlib.h>
 #include <crtdbg.h>
 #include <iostream>
@@ -13,14 +13,17 @@
 
 
 int main() {
-    _CrtMemState _ms;
+    /*_CrtMemState _ms;
     _CrtMemCheckpoint(&_ms);
     {
         auto client = ConsoleClient::CreateDefault();
         client.RunCommand("login");
         client.RunCommand("createaccount");
     }
-    _CrtMemDumpAllObjectsSince(&_ms);
+    _CrtMemDumpAllObjectsSince(&_ms);*/
 
+    auto client = ConsoleClient::CreateDefault();
+    client.RunCommand("login");
+    client.RunCommand("createaccount");
     return 0;
 }
